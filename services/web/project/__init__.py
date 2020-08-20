562093901e26b7b032a141b0198e9e7c8e1785fb
fwrhine <aghniaputriprawira@gmail.com>
2020-08-21 00:01:41 +1000

    change all db to team_clueless

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -23,10 +23,12 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
+    desc = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
 
-    def __init__(self, email):
+    def __init__(self, email, desc):
         self.email = email
+        self.desc = desc
 
 
 @app.route("/")
