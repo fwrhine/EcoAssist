58aa2dec1a8509ed6b5c08cdea089822e92bebfb
kiki rahmadi <kiki.rahmadi@hotmail.com>
2020-10-12 14:36:16 +1000

    update login and student task interaction

--- services/web/project/forms.py
+++ services/web/project/forms.py
@@ -3,6 +3,16 @@ from wtforms import StringField, TextAreaField, IntegerField, SubmitField, Selec
 from wtforms.validators import DataRequired, InputRequired, Email, ValidationError
 from .models import User, TeacherClasses
 
+# def invalid_credentials(form, field):
+#     email = form.email.data
+#     password = field.data
+#     user = User.query.filter_by(email=form.email.data).first()
+#     if user is None:
+#         ValidationError("Email is not registered")
+#     else:
+#         check_password = User.query.filter_by(email=form.email.data, password=form.password.data).first()
+#         if check_password is None:
+#            ValidationError("Wrong password")
 
 class TaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
@@ -36,7 +46,24 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
-
+    def validate(self):
+        if self.email.data and self.password.data:
+            user = User.query.filter_by(email=self.email.data).first()
+            if user is None:
+                self.email.errors += (ValidationError("Email is not registered"),)
+                return False
+            else:
+                check_password = User.query.filter_by(email=self.email.data, password=self.password.data).first()
+                if check_password is None:
+                    self.password.errors += (ValidationError("Wrong password"),)
+                    return False
+        else:
+            if not self.email.data:
+                self.email.errors += (ValidationError("This field is required"),)
+            if not self.password.data:
+                self.password.errors += (ValidationError("This field is required"),)
+            return False
+        return True
 
 class ClassForm(FlaskForm):
     class_name = StringField('Class Name', validators=[DataRequired()])
