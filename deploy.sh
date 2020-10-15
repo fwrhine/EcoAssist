dab242f4832c1ada13cb90bdea279cf0cbfa989d
fwrhine <aghniaputriprawira@gmail.com>
2020-10-15 22:12:27 +0000

    modify restart_service.sh with scaling

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-docker build --file ./services/web/Dockerfile.prod --tag aghniaprawira/team-clueless:latest ./services/web/
-docker build --file ./services/nginx/Dockerfile --tag aghniaprawira/team-clueless:nginx ./services/nginx
-docker push aghniaprawira/team-clueless:latest
-docker push aghniaprawira/team-clueless:nginx
-# scp -r -i team-clueless.pem ubuntu@18.216.185.212:/home/ubuntu/team-clueless
-# scp -i team-clueless.pem ./docker-compose.prod.yml ./restart_service.sh ubuntu@18.216.185.212:/home/ubuntu/team-clueless
-# ssh -i team-clueless.pem ubuntu@18.216.185.212 bash team-clueless/restart_service.sh
--- restart_service.sh
+++ restart_service.sh
@@ -1,3 +1,2 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $(echo $DIR)
-sudo docker-compose -f docker-compose.prod.yml down --rmi all && sudo docker-compose -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans
+sudo docker-compose -f docker-compose.prod.yml down --rmi all
+sudo docker-compose -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans --scale redis-master=1 --scale redis-replica=3 --scale web=3 --scale nginx=1
