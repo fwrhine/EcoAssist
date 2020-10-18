70218d2d794e4b4962768605b7f16f08caff0aaf
fwrhine <aghniaputriprawira@gmail.com>
2020-10-19 08:37:04 +1000

    add comment

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://") # uses SQLite if PostgreSQL server not available
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
