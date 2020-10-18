495cd5ba80f4ee9d22a516e1bc49a13c64580b5b
fwrhine <aghniaputriprawira@gmail.com>
2020-10-19 09:09:41 +1000

    nginx deploy settings

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker build --file ./services/nginx/Dockerfile --tag aghniaprawira/team-clueles
 docker push aghniaprawira/team-clueless:latest
 docker push aghniaprawira/team-clueless:nginx
 # scp -r -i team-clueless.pem ubuntu@18.216.185.212:/home/ubuntu/team-clueless
-# scp -i team-clueless.pem ./docker-compose.prod.yml ./restart_service.sh ubuntu@18.216.185.212:/home/ubuntu/team-clueless
+# ssh -i team-clueless.pem ubuntu@18.216.185.212:/home/ubuntu/team-clueless git pull
 # ssh -i team-clueless.pem ubuntu@18.216.185.212 bash team-clueless/restart_service.sh
